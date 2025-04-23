5683a9a1943e9de6a41c3fd5e692c504a058de58
ulka-shpulka <fon.003@mail.ru>
2025-04-23 17:40:47 +0600

    server.py

--- server.py
+++ server.py
@@ -36,4 +36,5 @@ def submit_visit():
         return jsonify({"error": "Не удалось отправить сообщение"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
